b98aa2205f65a7663e5e5dd864bf4fd531851e85
Zaczek23347 <92017943+Zaczek23347@users.noreply.github.com>
2024-03-30 16:13:03 +0100

    User Authentication
    Dodano permisje, oraz zaktualizowano model request

--- lyricalapp/request/views.py
+++ lyricalapp/request/views.py
@@ -3,7 +3,7 @@ import django_filters
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny, IsAuthenticated
 
 from .serializers import RequestSerializer
 from .models import Request
@@ -12,7 +12,7 @@ from .models import Request
 class RequestViewSet(viewsets.ModelViewSet):
     serializer_class = RequestSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
     queryset = Request.objects.all()
     ordering_fields = ['date']
     filter_backends = (django_filters.rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter)
