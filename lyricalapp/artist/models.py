58d93777d26ecf1699ab2fce873b9ec862937240
Jakub Karcz <FazKarcz2@wp.pl>
2024-04-04 22:57:34 +0200

    p

--- lyricalapp/artist/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Artist(models.Model):
-    nickname = models.CharField(max_length=256, unique=True)
-    origin = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.nickname
--- lyricalapp/lyricalapp/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def startingPage(request):
-    return render(request, 'index.html')
