a3803de4235f20da4f836a062a0257733901b5f7
Jakub Karcz <FazKarcz2@wp.pl>
2024-04-04 22:58:05 +0200

    Swagger

---
+++ lyricalapp/artist/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+import django_filters
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from .models import Artist
+from .serializers import ArtistSerializer
+
+
+class ArtistViewSet(viewsets.ModelViewSet):
+    serializer_class = ArtistSerializer
+    authentication_classes = (SessionAuthentication,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Artist.objects.all()
+    search_fields = ['surname', 'origin']
+    ordering_fields = ['nickname']
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter)
+
+
+def artistList(request):
+    artists = Artist.objects.all()
+    return render(request, 'artist_list.html', {'artists': artists})
---
+++ lyricalapp/song/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from artist.models import Artist
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, default='Brak')
+
+    def __str__(self):
+        return self.name
+
+
+class Album(models.Model):
+    album_name = models.CharField(max_length=255)
+    number_of_songs = models.IntegerField()
+    duration = models.FloatField()
+    release_date = models.DateField()
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)  # Po ID z tabeli artist
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, default=0)  # Domyślna wartość dla pola genre
+
+    def __str__(self):
+        return self.album_name
+
+
+class Song(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)  # Po ID z tabeli artist
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, default=0)  # Domyślna wartość dla pola genre
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, default=0)
+    song_name = models.CharField(max_length=50)
+    lyrics = models.TextField()
+    link = models.CharField(max_length=255, default=None)
+    release_date = models.DateField()  # Data wydania
+    update_date = models.DateField()
+
+    def __str__(self):
+        return self.song_name
