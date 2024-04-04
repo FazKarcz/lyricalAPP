886868862863c7ba49b0ebac59f9205a067f3d4c
Jakub Karcz <92019651+FazKarcz@users.noreply.github.com>
2024-04-05 00:04:32 +0200

    Update models.py

--- lyricalapp/request/models.py
+++ lyricalapp/request/models.py
@@ -5,3 +5,4 @@ class Request(models.Model):
     author = models.CharField(max_length=25)
     song_name = models.CharField(max_length=35)
     date = models.DateField(auto_now=True)
+    status = models.BooleanField(default=False)
