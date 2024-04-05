a1401ce614f91748d010ad97149d05e845818adf
Jakub Karcz <92019651+FazKarcz@users.noreply.github.com>
2024-04-05 08:41:50 +0200

    Update models.py

--- lyricalapp/artist/models.py
+++ lyricalapp/artist/models.py
@@ -2,11 +2,9 @@ from django.db import models
 
 
 class Artist(models.Model):
-    nickname = models.CharField(max_length=256, unique=True)
-    name = models.CharField(max_length=20)
-    surname = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=50)
     origin = models.CharField(max_length=20)
     date_of_birth = models.DateField()
 
     def __str__(self):
-        return self.name + ' ' + self.surname
+        return self.nickname
