005c4dbb31cf5a3eaf79e1425115f72138f0e18a
Sulgard <artur.sutula90@gmail.com>
2024-04-09 11:24:11 +0200

    added comment model and sulgardtest user for development purposes

--- lyricalapp/comment/models.py
+++ lyricalapp/comment/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from song.models import Song
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Comment(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.content
