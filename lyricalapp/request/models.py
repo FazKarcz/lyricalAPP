7bf317ac68f49bdfe081c60b604492560def6ecd
Sulgard <artur.sutula90@gmail.com>
2024-04-09 21:04:42 +0200

    made changes in request app admin files, modified request model

--- lyricalapp/request/models.py
+++ lyricalapp/request/models.py
@@ -6,3 +6,7 @@ class Request(models.Model):
     song_name = models.CharField(max_length=35)
     date = models.DateField(auto_now=True)
     status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.song_name + " - " + self.author
+
