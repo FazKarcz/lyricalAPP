73dd7589b4016446622fb23507efd55eb7c16d59
Sulgard <artur.sutula90@gmail.com>
2024-04-11 20:38:41 +0200

    Add request user form view and template

--- lyricalapp/request/models.py
+++ lyricalapp/request/models.py
@@ -1,12 +1,28 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Request(models.Model):
+
+    PENDING = 'Nie rozpatrzony'
+    ACCEPTED = 'Przyjęty'
+    DECLINED = 'Odrzucony'
+    STATUS_CHOICES = [
+        (PENDING, 'Nie rozpatrzony'),
+        (ACCEPTED, 'Przyjęty'),
+        (DECLINED, "Odrzucony")
+    ]
     author = models.CharField(max_length=25)
     song_name = models.CharField(max_length=35)
     date = models.DateField(auto_now=True)
-    status = models.BooleanField(default=False)
+    status = models.CharField(
+        max_length=15,
+        choices=STATUS_CHOICES,
+        default=PENDING,
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-status']
 
     def __str__(self):
-        return self.song_name + " - " + self.author
-
+        return self.song_name + " - " + self.author + " - " + self.status
--- lyricalapp/user/urls.py
+++ lyricalapp/user/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.dashboard, name="dashboard"),  #widok konta
     path('login', views.login, name="login"),  #widok logowania
     path('register', views.register, name="register"),  #widok rejestracji
-    path('logout', views.logout, name="logout")  #widok logout
-
+    path('logout', views.logout, name="logout"),  #widok logout
+    path('request_list', views.request_list, name="request_list"), #widok zgloszen uzytkownika
+    path('make_request', views.make_request, name="make_request") #widok formularza zgłoszen
 ]
