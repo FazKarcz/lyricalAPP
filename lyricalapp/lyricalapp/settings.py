297fb9ce2cc9e767f39f17b41076bdabb43acbea
StanleyN0vak <darkmarek2@gmail.com>
2024-05-05 18:55:33 +0200

    fix css in songDetails

--- lyricalapp/lyricalapp/settings.py
+++ lyricalapp/lyricalapp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,8 +127,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/static/css",
+    os.path.join(BASE_DIR, 'static')
 ]
 
 STATIC_ROOT = "/static/"
