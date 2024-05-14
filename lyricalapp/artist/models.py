6cd9f5c160a476e6013636488b96f377310d5327
Sulgard <artur.sutula90@gmail.com>
2024-05-14 22:27:43 +0200

    fixing a typo

--- lyricalapp/artist/models.py
+++ lyricalapp/artist/models.py
@@ -5,7 +5,7 @@ class Artist(models.Model):
     nickname = models.CharField(max_length=50)
     origin = models.CharField(max_length=20)
     date_of_birth = models.DateField()
-    poster = models.ImageField(upload_to='poster/', null=True, blank=True)
+    poster = models.ImageField(upload_to='posters/', null=True, blank=True)
 
     def __str__(self):
         return self.nickname
