962de819d30bf1ac20df4d4aa787c7f8330c6463
Sulgard <artur.sutula90@gmail.com>
2024-05-16 17:27:52 +0200

    nuke

--- lyricalapp/artist/models.py
+++ lyricalapp/artist/models.py
@@ -5,7 +5,6 @@ class Artist(models.Model):
     nickname = models.CharField(max_length=50)
     origin = models.CharField(max_length=20)
     date_of_birth = models.DateField()
-    poster = models.ImageField(upload_to='posters/', null=True, blank=True)
 
     def __str__(self):
         return self.nickname
