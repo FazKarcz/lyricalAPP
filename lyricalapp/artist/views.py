e6111c8685a6a5f263f10c9f1cbe921526268194
StanleyN0vak <darkmarek2@gmail.com>
2024-06-06 10:54:56 +0200

    Fix nav in all

--- lyricalapp/artist/views.py
+++ lyricalapp/artist/views.py
@@ -25,12 +25,13 @@ class ArtistViewSet(viewsets.ModelViewSet):
 def artistList(request):
     artists = Artist.objects.all()
     search_query = request.GET.get('serach_query')
-    paginator = Paginator(artists, 16)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
 
     if search_query:
         artists = artists.filter(Q(nickname__icontains=search_query))
+
+    paginator = Paginator(artists, 16)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
         'artists': page_obj.object_list,
